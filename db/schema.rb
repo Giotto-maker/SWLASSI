fceb4c4a798816a4ba67a1ac5b7e189e57e81d4d
Giotto-maker <luca.andolfi03@gmail.com>
2020-07-31 11:17:14 +0200

    Signup fields added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_191839) do
+ActiveRecord::Schema.define(version: 2020_07_31_073520) do
 
   create_table "artworks", force: :cascade do |t|
     t.integer "voto"
@@ -52,6 +52,11 @@ ActiveRecord::Schema.define(version: 2020_07_30_191839) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "surname"
+    t.string "username"
+    t.string "category"
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
