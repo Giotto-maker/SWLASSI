5572823496689fe5d3a70f73c4901518b95c3cf1
Giotto-maker <luca.andolfi03@gmail.com>
2020-07-31 11:44:35 +0200

    Confirmation emails

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,9 @@
+class RegistrationsController < Devise::RegistrationsController
+
+    private
+  
+    def after_inactive_sign_up_path_for(resource)
+      new_user_confirmation_path
+    end
+end
+  
