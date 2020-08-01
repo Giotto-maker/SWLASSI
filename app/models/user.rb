4d5bdc6289345b09b9bc99b147a364d570514c8e
ValentinaFerrazzi <63494510+ValentinaFerrazzi@users.noreply.github.com>
2020-08-01 18:01:50 +0200

    piccole modifiche

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable,
-         :omniauthable, omniauth_providers: %i[facebook]
+         :omniauthable, :omniauth_providers => [:facebook]
 
 
         def self.from_omniauth(auth)
